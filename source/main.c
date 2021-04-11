e860adc68ef57829b9785116c9ae53054ad043a4
Marod Jackson <mjack016@wch136-24.cs.ucr.edu>
2021-04-10 19:41:26 -0700

    started part 1

--- source/main.c
+++ source/main.c
@@ -13,11 +13,25 @@
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
-
-    /* Insert your solution below */
+    /* Insert DDR and PORT initializations /
+ *         DDRA = 0x00; PORTA = 0xFF;
+ *                 DDRB = 0x00; PORTB = 0xFF;
+ *                         DDRC = 0xFF; PORTC = 0x00;
+ *                             / Insert your solution below */
+        unsigned char cnt = 0x00;
+        unsigned char curr1 = 0x00;
+        unsigned char curr2 = 0x00;
+        unsigned char i;
+        unsigned char tmpA = PINA;
+        unsigned char tmpB = PINB;
     while (1) {
-
+        for(i = 0; i < 8; ++i){
+                curr1 = (tmpA >> i) & 0x01;
+                curr2 = (tmpB >> i) & 0x01;
+                cnt = cnt + curr1 + curr2;
+        }
+        PORTC = cnt;
     }
     return 1;
+
 }
